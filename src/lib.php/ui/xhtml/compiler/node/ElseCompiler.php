5ecb9b8ecc3443dc688c0dd29a78e19117cc61e0
kongyuan <kongyuan@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-05-20 09:40:57 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/framework@2493 fb4ae8c7-ff60-4932-ad58-89b0e700feac

---
+++ src/lib.php/ui/xhtml/compiler/node/ElseCompiler.php
@@ -0,0 +1,24 @@
+<?php
+namespace jc\ui\xhtml\compiler\node ;
+
+use jc\ui\xhtml\Node;
+use jc\lang\Type;
+use jc\ui\ICompiler;
+use jc\io\IOutputStream;
+use jc\ui\CompilerManager;
+use jc\ui\IObject;
+use jc\ui\xhtml\compiler\NodeCompiler;
+
+class ElseCompiler extends NodeCompiler 
+{
+	public function compile(IObject $aObject,IOutputStream $aDev,CompilerManager $aCompilerManager)
+	{
+		Type::check("jc\\ui\\xhtml\\Node",$aObject) ;
+
+		$aDev->write("<?php
+					}else{
+					?>");
+	}
+}
+
+?>
