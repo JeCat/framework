6df83301e1e86b96a2e73c6407c1ba0cd925b18d
kongyuan <kongyuan@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-06-03 06:29:45 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/framework@2534 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- src/lib.php/ui/xhtml/compiler/node/ForeachCompiler.php
+++ src/lib.php/ui/xhtml/compiler/node/ForeachCompiler.php
@@ -2,91 +2,84 @@
 /**
  * foreach
  * 
+ * 成对写法:
  * <foreach exp>
- * 	[<foreach:else/>]
+ * [<foreach:else/>]
  * </foreach>
+ * 
+ * 单行写法:
+ * <foreach exp />
+ * 	[<foreach:else/>]
+ * <foreach/>
  * 
  * @author anubis
  *
  */
-namespace jc\ui\xhtml\compiler\node ;
+namespace jc\ui\xhtml\compiler\node;
 
 use jc\ui\xhtml\Node;
 use jc\lang\Type;
+use jc\lang\Exception;
 use jc\ui\ICompiler;
 use jc\io\IOutputStream;
 use jc\ui\CompilerManager;
 use jc\ui\IObject;
 use jc\ui\xhtml\compiler\NodeCompiler;
 
-class ForeachCompiler extends NodeCompiler 
-{
-	public function compile(IObject $aObject,IOutputStream $aDev,CompilerManager $aCompilerManager)
-	{
-		Type::check("jc\\ui\\xhtml\\Node",$aObject) ;
-
-		//为变量名准备唯一的标示,防止foreach嵌套后出现冲突
-		$sObjId = spl_object_hash($aObject);
+class ForeachCompiler extends NodeCompiler {
+	public function compile(IObject $aObject, IOutputStream $aDev, CompilerManager $aCompilerManager) {
 		
+		Type::check("jc\\ui\\xhtml\\Node", $aObject );
 		
+		$aAttrs = $aObject->attributes();
 		
-		$aAttrs = $aObject->attributes() ;
-		$sFor = $aAttrs->expression('for');
-		$bHasKey = $aAttrs->has('key');
-		$bHasItem = $aAttrs->has('item');
-		$sKey = $bHasKey? $aAttrs->get('key') : NodeCompiler::assignVariableName('$__foreach_key_');
-		$sItem = $bHasItem? $aAttrs->get('item') : NodeCompiler::assignVariableName('$__foreach_item_');
-		$sDesc = $aAttrs->has('desc')? $aAttrs->get('desc') : 'false';    //是否反序
-		$sArrName = NodeCompiler::assignVariableName('$__foreach_Arr_');
-		
-		$aDev->write("<?php
-				$sArrName = $sFor;
-				if(!empty($sArrName)){
-					if($sDesc){
-					 	$sArrName = array_reverse($sArrName);
-					}
-					");
-		if($bHasKey && $bHasItem){
-			if(substr($sKey,0,1) == '$'){
-				$sKeyName = substr($sKey,1);
-			}else{
-				$sKeyName = $sKey;
-				$sKey = '$' . $sKey;
+		if( $aAttrs->has ( 'for' ) ){
+			$sFor = $aAttrs->expression ( 'for' );
+			if( substr($sFor, 0 , 1) == '$' ){
+				$sFor = substr($sFor, 1);
 			}
-			if(substr($sItem,0,1) == '$'){
-				$sItemName = substr($sItem,1);
-			}else{
-				$sItemName = $sItem;
-				$sItem = '$' . $sItem;
-			}
-			$aDev->write("
-					foreach({$sArrName}as ($sKey => )$sItem){
-						\$aVariables->set('$sKeyName',$sKey);
-						\$aVariables->set('$sItemName',$sItem);
-						");
-		}elseif(!$bHasKey && $bHasItem){
-			if(substr($sItem,0,1) == '$'){
-				$sItemName = substr($sItem,1);
-			}
-			$aDev->write("
-					foreach($sArrName as \${$sItem}){
-						\$aUI->variables()->set('$sItemName',$sItem);
-						");
-		}elseif(!$bHasKey && !$bHasItem){
-			$aDev->write("
-					foreach($sArrName as $sKey => $sItem){
-						");
+//			var_dump($sFor);
+//			exit();
+		}else{
+			throw new Exception("foreach tag can not run without 'for' attribute");
 		}
-		$aDev->write("?>");			
-				
-		//循环体，可能会包含foreach:else标签
-		$this->compileChildren($aObject,$aDev,$aCompilerManager) ;
+		
+		$bIsSingle = $aObject->headTag()->isSingle() ? true : false ;
+		
+		$sKey = $aAttrs->has ( 'key' ) ? $aAttrs->get ( 'key' ) : '' ;//: NodeCompiler::assignVariableName ( '__foreach_key_' );
+		$sItem = $aAttrs->has ( 'item' ) ? $aAttrs->get ( 'item' ) : NodeCompiler::assignVariableName ( '__foreach_item_' );
+//		$sArrName = NodeCompiler::assignVariableName ( '__foreach_Arr_' );
+		//\${$sArrName} = {$sFor};
+		$aDev->write ( "<?php
+				if(!empty(\${$sFor})){
+					" );
+		$aDev->write ( "
+					foreach($sFor as ");
+		if($sKey){
+			$aDev->write ( " \${$sKey} => " ) ;
+		}
+		
+		$aDev->write ( " \${$sItem} ){ 
+						");
+		if($sKey){
+			$aDev->write ( " \$aVariables->set('{$sKey}',\${$sKey}); ");
+		}
+		
+		$aDev->write ( " \$aVariables->set('{$sItem}',\${$sItem} ); ");				
+						
+		
+		$aDev->write("?>");
+		
+		//是否是单行标签?
+		if(!$aObject->headTag()->isSingle()){
+			//循环体，可能会包含foreach:else标签
+			$this->compileChildren($aObject,$aDev,$aCompilerManager) ;
+		}
 		
 		$aDev->write("<?php 
 					}
 				}
 			 		?>") ; // end if   (如果foreach的内容包含foreach:else标签,则此处为foreach:else的end)
-		
 	}
 }
 
