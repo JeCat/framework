8946f300768bb6b91b605728f7dc6a6338b99f1c
alee <alee@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-06-08 10:42:32 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/framework@2573 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- src/template/compileds/ViewWidgetText.template.html.php
+++
@@ -1,4 +0,0 @@
-<input id="<?php echo eval("if(!isset(\$__uivar_theWidget)){ \$__uivar_theWidget=&\$aVariables->getRef('theWidget') ;};
-return \$__uivar_theWidget->id();") ;?>" name="<?php echo eval("if(!isset(\$__uivar_theWidget)){ \$__uivar_theWidget=&\$aVariables->getRef('theWidget') ;};
-return \$__uivar_theWidget->formName();") ;?>" value="<?php echo eval("if(!isset(\$__uivar_theWidget)){ \$__uivar_theWidget=&\$aVariables->getRef('theWidget') ;};
-return \$__uivar_theWidget->valueToString();") ;?>" />
