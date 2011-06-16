1b15447300988726e0209acbd95591a37cc9561a
kongyuan <kongyuan@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-06-16 09:02:44 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/framework@2608 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- src/lib.php/verifier/Same.php
+++ src/lib.php/verifier/Same.php
@@ -6,7 +6,7 @@ use jc\mvc\view\widget\Text;
 use jc\lang\Exception;
 use jc\lang\Object;
 
-class ConsistenceEqualCheck extends Object implements IVerifier {
+class Same extends Object implements IVerifier {
 	public function __construct() {
 	}
 	
@@ -15,17 +15,14 @@ class ConsistenceEqualCheck extends Object implements IVerifier {
 			throw new Exception ( __CLASS__ . "的" . __METHOD__ . "传入了错误的data参数(得到的参数是:%s)", array ($data ) );
 		}
 		
-		foreach($data as $value){
-			if( $data[0] != $value ){
-				if ( $bThrowException ) {
-					throw new VerifyFailed ( "校验的值不相等" );
-				}
-				return false;
+		if( count(array_unique($data)) != 1){
+			if ( $bThrowException ) {
+				throw new VerifyFailed ( "输入内容必须一致" );
 			}
+			return false;
 		}
 		return true;
 	}
-	
 }
 
 ?>
