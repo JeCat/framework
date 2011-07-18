9914cf6f22c113db6054a3c1ea5bcb749d60f012
alee <alee@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-07-18 04:10:58 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/framework@2749 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- src/lib.php/mvc/view/uicompiler/FormCompiler.php
+++ src/lib.php/mvc/view/uicompiler/FormCompiler.php
@@ -16,13 +16,13 @@ class FormCompiler extends NodeCompiler
 		
 		if( $aTailTag = $aObject->tailTag() )
 		{
-			$aDev->write("<?php if( !(\$aVariables->get('theView') instanceof \\jc\\mvc\\view\\FormView) or \$aVariables->get('theView')->isShowForm() ) { ?>\r\n") ;
+			$aDev->write("<?php if( !(\$aVariables->get('theView') instanceof \\jc\\mvc\\view\\IFormView) or \$aVariables->get('theView')->isShowForm() ) { ?>\r\n") ;
 			
 			$this->compileTag($aObject->headTag(), $aDev, $aCompilerManager) ;
 
 			$this->compileChildren($aObject, $aDev, $aCompilerManager) ;
 
-			$aDev->write('<input type="hidden" name="<?php echo $aVariables->get(\'theView\')->htmlFormSignature()?>" value="1" />') ;
+			$aDev->write('<?php if(\$aVariables->get(\'theView\') instanceof \\jc\\mvc\\view\\IFormView){ ?><input type="hidden" name="<?php echo $aVariables->get(\'theView\')->htmlFormSignature()?>" value="1" /><?php } ?>') ;
 			$this->compileTag($aTailTag, $aDev, $aCompilerManager) ;
 			
 			$aDev->write("<?php } ?>\r\n") ;
