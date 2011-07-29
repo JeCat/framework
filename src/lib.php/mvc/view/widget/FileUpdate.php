fff08dbc2ddfbead7f7025b71e1094ce265bbf0c
kongyuan <kongyuan@fb4ae8c7-ff60-4932-ad58-89b0e700feac>
2011-07-29 07:29:09 +0000

    git-svn-id: http://svn.jecat.cn/framework/0.6/trunk/framework@2794 fb4ae8c7-ff60-4932-ad58-89b0e700feac

--- src/lib.php/mvc/view/widget/FileUpdate.php
+++ src/lib.php/mvc/view/widget/FileUpdate.php
@@ -2,7 +2,7 @@
 namespace jc\mvc\view\widget;
 
 use jc\mvc\view\DataExchanger;
-
+use jc\lang\Type;
 use jc\lang\Exception;
 use jc\system\Request;
 use jc\mvc\view\IView;
@@ -11,15 +11,19 @@ use jc\util\IDataSrc;
 use jc\fs\IFolder;
 use jc\fs\archive\IAchiveStrategy;
 use jc\fs\archive\DateAchiveStrategy;
+use jc\fs\IFile;
 
 class FileUpdate extends FormWidget{
 	
 	public function __construct($sId, $sTitle = null , IFolder $aFolder , IAchiveStrategy $aAchiveStrategy = null ,  IView $aView = null) {
 		if (empty($aFolder)) {
-			throw new Exception ( "构建" . __CLASS__ . "对象时使用了非法的aFolder参数(得到的aFolder是:%s)", array ($aFolder ) );
+			throw new Exception ( "构建" . __CLASS__ . "对象时使用了非法的aFolder参数(得到的aFolder是:%s类型)", array (Type::detectType($aFolder) ) );
 		}
+		$this->aFolder = $aFolder;
 		if($aAchiveStrategy == null){
-			$aAchiveStrategy = DateAchiveStrategy::flyweight(Array(true,true,true));
+			$this->aAchiveStrategy = DateAchiveStrategy::flyweight(Array(true,true,true));
+		}else{
+			$this->aAchiveStrategy = $aAchiveStrategy;
 		}
 		parent::__construct ( $sId, 'jc:WidgetFileUpdate.template.html', $sTitle, $aView );
 	}
@@ -33,7 +37,6 @@ class FileUpdate extends FormWidget{
 		return false;
 	}
 	public function getFileName(){
-		//TODO 新建?文件上传类的对象,得到虚拟文件路径A,组成真实文件下载地址,以便用户可以下载
 		return 'fdsfds';
 	}
 	public function getFileSize(){
@@ -50,7 +53,22 @@ class FileUpdate extends FormWidget{
 	}
 	
 	public function valueToString() {
+		$this->file();
 		return strval ( $this->value () );
+	}
+	
+	public function file(){
+		if($this->aFile == null){
+			//转存文件到网站的文件目录下
+			if($this->aUploadedFile == null){
+				throw new Exception ( __CLASS__ . "的" . __METHOD__ . "方法没有找到上传来的文件" );
+			}
+			$aFileSavePath = $this->aAchiveStrategy->makePath($this->aUploadedFile, $this->aFolder);
+			var_dump($aFileSavePath);
+			exit();
+			$this->aFile = $this->aUploadedFile->move($aFileSavePath);
+		}
+		return $this->aFile;
 	}
 	
 	public function setValueFromString($data) {
@@ -58,14 +76,24 @@ class FileUpdate extends FormWidget{
 	}
 	
 	public function setDataFromSubmit(IDataSrc $aDataSrc) {
-		//创建文件上传管理器
-		
+		$uploadedFile = $aDataSrc->get ( $this->formName ());
+		if(! $uploadedFile instanceof IFile){
+			throw new Exception ( __CLASS__ . "的" . __METHOD__ . "传入了错误的参数(得到的参数是%s类型)", array ( Type::detectType($uploadedFile) ) );
+		}
+		$this->aUploadedFile = $uploadedFile;
 		$this->setValueFromString ( $aDataSrc->get ( $this->formName () ));
-//		var_dump($aDataSrc->get ( $this->formName ()));
-		//$this->setValueFromString ( $aDataSrc->get ( $this->formName () ) );
 	}
 	
 	private $sStoreDir;
+	private $aAchiveStrategy;
+	private $aFolder;
+	
+	/**
+	 * @var	jc\fs\imp\UploadFile
+	 */
+	private $aUploadedFile = null;
+	
+	private $aFile = null;
 }
 
 ?>
---
+++ src/lib.php/verifier/FileSize.php
@@ -0,0 +1,36 @@
+<?php
+namespace jc\verifier;
+
+use jc\lang\Type;
+
+use jc\message\Message;
+
+use jc\lang\Exception;
+use jc\lang\Object;
+use jc\fs\IFile;
+
+class FileSize extends Object implements IVerifier {
+	public function __construct($nMaxSize) {
+		$nMaxSize = (int)$nMaxSize;
+		if( $nMaxSize > 0){
+			$this->nMaxSize = $nMaxSize;
+		}
+	}
+	
+	public function verify($data, $bThrowException) {
+		if (! $data instanceof IFile) {
+			throw new Exception ( __CLASS__ . "的" . __METHOD__ . "传入了错误的data参数(得到的参数是%s类型)", array ( Type::detectType($data) ) );
+		}
+		$nDataSize = $data->length() ;
+		if ($nDataSize > $this->nMaxSize ) {
+			if ($bThrowException) {
+				throw new VerifyFailed ( "上传的文件大小大于网站限制" );
+			}
+			return false;
+		}
+		return true;
+	}
+	
+	private $nMaxSize;
+}
+?>
