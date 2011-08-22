6a52a36491c9dc1c93707341d2c9a3e802461ffe
zq@alee2002.com <myname@server>
2011-08-22 15:07:04 +0800

    完成 jc\compile\interpreters\oop\NamespaceParser 测试

--- src/lib.php/compile/interpreters/oop/NamespaceParser.php
+++ src/lib.php/compile/interpreters/oop/NamespaceParser.php
@@ -1,13 +1,39 @@
 <?php
 namespace jc\compile\interpreters\oop ;
 
+use jc\compile\object\NamespaceDeclare;
+use jc\pattern\iterate\INonlinearIterator;
+use jc\compile\object\TokenPool;
 use jc\compile\object\Token;
 
 class NamespaceParser implements ISyntaxPaser
 {
-	public function parse(Token $aToken,State $aState)
+	public function parse(TokenPool $aTokenPool,INonlinearIterator $aTokenPoolIter,State $aState)
 	{
+		$aOriToken = $aTokenPoolIter->current() ;
+		$aTokenPoolIter = clone $aTokenPoolIter ;
 		
+		if( !$aOriToken or $aOriToken->tokenType()!=T_NAMESPACE )
+		{
+			return ;
+		}
+		
+		$aNewToken = new NamespaceDeclare($aOriToken) ;
+		
+		for( 
+			$aTokenPoolIter->next();
+			$aToken=$aTokenPoolIter->current() and $aToken->tokenType()!=Token::T_SEMICOLON;
+			$aTokenPoolIter->next()
+		)
+		{
+			if( $aToken->tokenType()==T_STRING )
+			{
+				$aNewToken->addNameToken($aToken) ;
+			}
+		}
+		
+		$aTokenPool->replace($aNewToken, $aOriToken) ;
+		$aState->setCurrentNamespace($aNewToken) ;
 	}
 }
 
