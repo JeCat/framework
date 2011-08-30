e2394e79d7659f7545489b5d624449e2e8087776
kongyuan <anubiskong@gmail.com>
2011-08-30 18:38:00 +0800

    新的Criteria类,以及新增的mvc/model/db/orm/Criteria

---
+++ src/lib.php/mvc/model/db/orm/Criteria.php
@@ -0,0 +1,28 @@
+<?php
+namespace jc\mvc\model\db\orm ;
+
+use jc\db\sql\Criteria as ParentCriteria;
+
+class Criteria extends ParentCriteria {
+	public function setDefaultTable($sDefaultTable) {
+		$this->sDefaultTable = (string)$sDefaultTable;
+	}
+	
+	public function defaultTable() {
+		return $this->sDefaultTable;
+	}
+
+	protected function transColumn($sColumn)
+	{
+		// 在没有表名的字段前 添加默认表名
+		if( $this->sDefaultTable and strstr($sColumn,'.')===false )
+		{
+			$sColumn = $this->makeSureBackQuote($this->sDefaultTable) . '.' . $this->makeSureBackQuote($sColumn) ;
+		}
+		
+		return $sColumn ;
+	}
+	
+	private $sDefaultTable = '';
+}
+?>
