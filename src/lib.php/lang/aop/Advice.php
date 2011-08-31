dc2f9086c5f594c358a7084b356f9c8b704ca999
zq@alee2002.com <myname@server>
2011-08-31 11:38:03 +0800

    + Advice::testCreateFromToken()

--- src/lib.php/lang/aop/Advice.php
+++ src/lib.php/lang/aop/Advice.php
@@ -1,6 +1,9 @@
 <?php
 namespace jc\lang\aop ;
 
+use jc\lang\Exception;
+
+use jc\lang\compile\object\FunctionDefine;
 use jc\pattern\composite\NamedObject;
 
 class Advice extends NamedObject
@@ -9,14 +12,63 @@ class Advice extends NamedObject
 	const before = 'before' ;
 	const after = 'after' ;
 	
-	static private $arrTypes = array(
+	static private $arrPositionTypes = array(
 		self::around, self::before, self::after
 	) ;
 	
-	public function __construct($fnSource,$sPosition=self::after)
+	public function __construct($sName,$fnSource,$sPosition=self::after)
 	{
+		if( !in_array($sPosition,self::$arrPositionTypes) )
+		{
+			throw new Exception("传入了无效的\$sPosition参数值：%s",$sPosition) ;
+		}
+		
+		parent::__construct($sName) ;
+		
 		$this->fnSource = $fnSource ;
 		$this->sPosition = $sPosition ;
+	}
+
+	static public function createFromToken(FunctionDefine $aFunctionDefine)
+	{
+		if( !$aClassDefine=$aFunctionDefine->belongsClass() )
+		{
+			throw new Exception("传入的 \$aFunctionDefine 参数无效，必须是一个类方法的定义Token") ;
+		}
+		
+		if( !$aDocToken = $aFunctionDefine->docToken() )
+		{
+				throw new Exception("传入了无效Advice %s::%s() ：没有DocComment申明。",array(
+					$aClassDefine->fullName()
+					, $aFunctionDefine->name()
+				)) ;
+		}
+		
+		$aDocComment = $aDocToken->docComment() ;
+
+		$sPosition = null ;
+		if( $aDocComment->hasItem('advice') )
+		{
+			$sPosition = $aDocComment->item('advice') ;
+			$sPosition = trim($sPosition) ;
+			$sPosition = strtolower($sPosition) ;
+		}
+		if(!$sPosition)
+		{
+			$sPosition = self::after ;
+		}
+
+		return new self($aFunctionDefine->name(),$aFunctionDefine->bodySource(),$sPosition) ;
+	}
+	
+	public function position()
+	{
+		return $this->sPosition ;
+	}
+	
+	public function source()
+	{
+		return $this->fnSource ;
 	}
 	
 	private $fnSource ;
