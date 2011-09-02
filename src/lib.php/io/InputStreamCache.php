4283b4e5a72d5e64e02e478b0bc28519702d2d5d
zq@alee2002.com <myname@server>
2011-09-02 16:54:53 +0800

    + InputStreamCache

---
+++ src/lib.php/io/InputStreamCache.php
@@ -0,0 +1,121 @@
+<?php
+namespace jc\io ;
+
+use jc\util\String;
+use jc\lang\Object;
+
+class InputStreamCache extends Object implements IInputStream
+{
+	public function __construct($sData='')
+	{
+		$this->setData($sData) ;
+	}
+	
+	/**
+	 * Enter description here ...
+	 * 
+	 * @return int
+	 */
+	function read($nBytes=-1)
+	{
+		$nDataLen = $this->available() ;
+		
+		if( $nBytes<0 )
+		{
+			$nBytes = $nDataLen ;
+		}
+		else 
+		{
+			if($nBytes>$nDataLen)
+			{
+				$nBytes = $nDataLen ;
+			}
+		}
+		
+		if( $nBytes<=0 )
+		{
+			return '' ;
+		}
+		
+		$nFrom = $this->nPostion ;
+		$this->nPostion+= $nBytes ;
+		
+		return substr($this->sData,$nFrom,$nBytes) ;
+	}
+	
+	/**
+	 * Enter description here ...
+	 * 
+	 * @return string
+	 */
+	function readInString(String $aString,$nBytes=-1)
+	{
+		$sBytes = $this->read($nBytes) ;
+		$aString->append( $sBytes ) ;
+		
+		return strlen($sBytes) ;
+	}
+	
+	/**
+	 * Enter description here ...
+	 * 
+	 * @return void
+	 */
+	function reset()
+	{
+		$this->nPostion = 0 ;
+	}
+	
+	/**
+	 * Enter description here ...
+	 * 
+	 * @return int
+	 */
+	function available()
+	{
+		return strlen($this->sData)-$this->nPostion ;
+	}
+	
+	/**
+	 * Enter description here ...
+	 * 
+	 * @return void
+	 */
+	function seek($nPosition)
+	{
+		if($nPosition<0)
+		{
+			$nPosition = -1 ;
+		}
+		
+		$this->nPosition = $nPosition ;
+	}
+	
+	/**
+	 * Enter description here ...
+	 * 
+	 * @return bool
+	 */
+	public function isEnd()
+	{
+		$this->nPostion == strlen($this->sData) ;
+	}
+
+
+	public function setData($sData)
+	{
+		$this->sData = $sData ;
+		
+		$this->reset() ;
+	}
+	public function data()
+	{
+		return $this->sData ;
+	}
+	
+	private $sData ;
+	
+	private $nPostion = 0 ;
+}
+
+?>
