3b76d585ed1aa2ac4ec2e55739df804597fbdcb4
alee <zq@alee2002.com>
2011-09-30 10:55:03 +0800

    start js framework

---
+++ src/lib.js/lang/Function.js
@@ -0,0 +1,16 @@
+jc.lang.Function = function ()
+{
+	this.__arrArgvs = [] ;
+}
+/*
+jc.lang.Function.define(
+	"xxxx"
+	, ['string', "abc"]
+	, []
+	, []
+	, function (xxd,sName,nYear){
+		
+	}
+) ;
+
+*/
---
+++ src/lib.js/lang/Namespace.js
@@ -0,0 +1,3 @@
+jc.lang.Namespace = {
+	
+}
