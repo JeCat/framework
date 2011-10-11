47129500eb65e67b4ef042ab9bfed813891e4e01
lexdene <elephant_liu@mail.dlut.edu.cn>
2011-10-11 15:52:07 +0800

    basely finish Selecter

--- src/lib.php/mvc/model/db/orm/Selecter.php
+++ src/lib.php/mvc/model/db/orm/Selecter.php
@@ -34,15 +34,16 @@ class Selecter extends Object{
         */
         $arrPrototypeTree = array();
         $arrPrototypeTree[0] = array(
-                            ['Prototype'] => $aPrototype,
-                            ['Table alias'] => $aPrototype->name(),
-                            ['association'] => array(),
-                            ['father node'] => -1
+                            'Prototype' => $aPrototype,
+                            'Table alias' => $aPrototype->name(),
+                            'association' => array(),
+                            'father node' => -1
                             );
         $arrPrototypeTree[0]['Table object'] = StatementFactory::createTable(
                                                                     $arrPrototypeTree[0]['Prototype']->tableName(),
                                                                     $arrPrototypeTree[0]['Table alias']
                                                                             );
+        $aSqlSelect -> addTable ( $arrPrototypeTree[0]['Table object']);
         $arrTravelQueue = array();
         $arrTravelQueue [] = 0;
         while( ! empty($arrTravelQueue)){
@@ -52,28 +53,78 @@ class Selecter extends Object{
             foreach($aTmpPrototype->associationIterator() as $aAssociation){
                 $nNext = count($arrTravelQueue);
                 $arrPrototypeTree[$nTmp]['association'][]=array(
-                                                            ['object'] => $aAssociation,
-                                                            ['node no'] => $nNext
+                                                            'object' => $aAssociation,
+                                                            'node no' => $nNext
                                                         );
                 $arrPrototypeTree[$nNext] = array(
-                                                ['Prototype'] => $aAssociation->toPrototype(),
-                                                ['Table alias'] => $aTmpTableAlias.'.'.$aAssociation->name(),
-                                                ['association'] => array(),
-                                                ['father node'] => $nTmp
+                                                'Prototype' => $aAssociation->toPrototype(),
+                                                'Table alias' => $aTmpTableAlias.'.'.$aAssociation->name(),
+                                                'association' => array(),
+                                                'father node' => $nTmp
                                             );
                 $arrPrototypeTree[$nNext]['Table object'] = StatementFactory::createTable(
                                                                     $arrPrototypeTree[$nNext]['Prototype']->tableName(),
                                                                     $arrPrototypeTree[$nNext]['Table alias']
                                                                             );
+                $aSqlSelect -> addTable ( $arrPrototypeTree[$nNext]['Table object']);
                 $arrTravelQueue[]=$nNext;
             }
         }
-        $arrTravelQueue = array();
-        $arrJoinQueue = array();
-        $arrTravelQueue [] = 0;
-        while( ! empty($arrTravelQueue)){
-            $nTmp = array_shift($arrTravelQueue);
-            $aTmpPrototype = $arrProrotypeTree[$nTmp]['Prototype'];
-            $aTmpTableAlias = $arrPrototypeTree[$nTmp]['Table alias'];
-            
+        $arrHeadQueue = array(0);
+        while( ! empty( $arrHeadQueue) ){
+            $nHead = array_shift( $arrHeadQueue );
+            $arrJoinQueue = array( $nHead );
+            while( !empty( $arrJoinQueue ) ){
+                $nJoin = array_shift( $arrJoinQueue );
+                $aNowTable = $arrPrototypeTree[$nJoin]['Table object'];
+                $arrAssociation = $arrPrototypeTree[$nJoin]['association'];
+                foreach( $arrAssociation as $association ){
+                    $nNode = $association['node no'];
+                    $aTablesJoin = StatementFactory::createTablesJoin();
+                    $aTablesJoin->addTable( $arrPrototypeTree[$nNode] ['Table object'] );
+                    $aNowTable -> addJoin($aTablesJoin);
+                    if( $association['object']->isType(Association::oneToOne)){
+                        $arrJoinQueue[]=$nNode;
+                    }else{
+                        $arrHeadQueue[]=$nNode;
+                    }
+                }
+            }
         }
+        $aSqlSelect -> addTable ( $arrPrototypeTree[0]['Table object'] );
+        $arrHeadQueue = array(0);
+        while( ! empty( $arrHeadQueue) ){
+            $nHead = array_shift( $arrHeadQueue );
+            $arrJoinQueue = array( $nHead );
+            $aHeadPrototype = $arrPrototypeTree[$nHead]['Prototype'];
+            $aSqlSelect -> clearColumn();
+            $aSqlSelect -> criteria()->setLimitFrom(
+                                                $aHeadPrototype->criteria()->limitFrom()
+                                                    );
+            $aSqlSelect -> criteria()->setLimitLen(
+                                                $aHeadPrototype->criteria()->limitLen()
+                                                    );
+            $aSqlSelect -> criteria() -> setOrder(
+                                                $aHeadPrototype->criteria()->orders()
+                                                    );
+            while( !empty( $arrJoinQueue ) ){
+                $nJoin = array_shift( $arrJoinQueue );
+                $strTableAlias = $arrPrototypeTree[$nJoin]['Table alias'];
+                $aNowPrototype = $arrPrototypeTree[$nJoin]['Prototype'];
+                foreach($aNowPrototype->columnIterator() as $column){
+                    $aSqlSelect -> addColumn( $column , $strTableAlias.$column);
+                }
+                foreach( $arrAssociation as $association ){
+                    $nNode = $association['node no'];
+                    if( $association['object']->isType(Association::oneToOne)){
+                        $arrJoinQueue[]=$nNode;
+                    }else{
+                        $arrHeadQueue[]=$nNode;
+                    }
+                }
+            }
+        }
+        $aRecordSet = $aDB->execute( $aSqlSelect->makeStatement() );
+    }
+}
+?>
