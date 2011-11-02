bc3021b0089692d461b68720810f09d5c54fb69f
alee <zq@alee2002.com>
2011-11-02 17:01:15 +0800

    refact-db-model: Inserter

--- src/lib.php/mvc/model/db/orm/Inserter.php
+++ src/lib.php/mvc/model/db/orm/Inserter.php
@@ -9,7 +9,7 @@ use jc\db\DB;
 use jc\mvc\model\db\IModel ;
 use jc\db\sql\StatementFactory ;
 
-class Inserter extends Object
+class Inserter extends OperationStrategy
 {
     public function execute(DB $aDB, IModel $aModel)
 	{
@@ -181,39 +181,6 @@ class Inserter extends Object
 			}
 		}
 	}
-	
-	function sss()
-    {
-        $aPrototype = $aModel->prototype();
-        $sTableName = $aPrototype ->tableName();
-        $aSqlInsert = StatementFactory::singleton()->createInsert($sTableName);
-        
-        foreach($aModel->dataIterator() as $alias => $data)
-        {
-            $column = $aPrototype->getColumnByAlias($alias) ;
-            $aSqlInsert->setData($column,$data) ;
-            $aModel->removeChange($alias) ;
-        }
-        $aDB->execute($aSqlInsert->makeStatement());
-        foreach($aModel->prototype()->associationIterator() as $aAssociation)
-        {
-            if( $aAssociation->isType(Association::hasAndBelongsTo) )
-            {
-                $aBridgeInsert = StatementFactory::singleton()->createInsert($aAssociation->bridgeTable()->name());
-                $toBridgeKeys = $aAssociation->toBridgeKeys();
-                $fromBridgeKeys = $aAssociation->fromBridgeKeys();
-                $fromKeys = $aAssociation->fromKeys();
-                $toKeys = $aAssociation->toKeys();
-                $n = count($toKeys);
-                for($i=0;$i<$n;++$i)
-                {
-                    $aBridgeInsert->setData($toBridgeKeys[$i],$aModel->data($fromKeys[$i]));
-                    $aBridgeInsert->setData($fromBridgeKeys[$i],$aModel->child($aAssociation->name())->data($toKeys[$i]));
-                }
-                $aDB->execute($aBridgeInsert->makeStatement());
-            }
-        }
-    }
 }
 
 ?>
