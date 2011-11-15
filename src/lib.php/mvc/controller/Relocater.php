e853143ee4bf2f997ced0a8092c513be11f76de7
alee <zq@alee2002.com>
2011-11-15 19:39:19 +0800

    Reloacater message

--- src/lib.php/mvc/controller/Relocater.php
+++ src/lib.php/mvc/controller/Relocater.php
@@ -2,14 +2,18 @@
 namespace jc\mvc\controller;
 
 
+use jc\system\Application;
+
+use jc\locale\Locale;
+
 class Relocater extends Controller
 {
 	const notBack = 'back://notBack' ;
 	const backToReferer = 'back://backToReferer' ;
 	
-	static public function locate($sUrl,$sTitle,$nWaitSec=3,IController $aParentController=null)
+	static public function locate($sUrl,$sMessage,$messageArgs=null,$nWaitSec=3,IController $aParentController=null)
 	{
-		$aRelocater = new self($sUrl,$sTitle,$nWaitSec) ;
+		$aRelocater = new self($sUrl,$sMessage,$messageArgs,$nWaitSec) ;
 		
 		// “恐龙妈妈”
 		if(!$aParentController)
@@ -45,7 +49,7 @@ class Relocater extends Controller
 		return $aRelocater ;
 	}
 	
-	public function __construct($sUrl,$sTitle,$nWaitSec=3)
+	public function __construct($sUrl,$sMessage,$messageArgs=null,$nWaitSec=3)
 	{
 		parent::__construct() ;
 	
@@ -62,7 +66,8 @@ class Relocater extends Controller
 			$this->sUrl = $sUrl ;
 		}
 		
-		$this->sTitle = $sTitle ;
+		$this->sMessage = $sMessage ;
+		$this->arrMessageArgs = $messageArgs? (array)$messageArgs: array() ;
 		$this->nWaitSec = $nWaitSec ;
 	}
 	
@@ -76,14 +81,26 @@ class Relocater extends Controller
 		$this->viewRelocater->enable() ;
 	}
 	
-	public function setTitle($sTitle)
+	public function setMessage($sMessage)
 	{
-		$this->sTitle = $sTitle ;
+		$this->sTitle = sMessage ;
 	}
 	
-	public function title()
+	public function messageSentence()
 	{
-		return $this->sTitle ;
+		return $this->sMessage ;
+	}
+	
+	public function message(ILocale $aLocale=null)
+	{
+		if( !$aLocale and $aLocaleMgr=Application::singleton()->localeManager() )
+		{
+			$aLocale = $aLocaleMgr->locale() ;
+		}
+		
+		return $aLocale?
+			$aLocale->trans($this->sMessage,$this->arrMessageArgs):
+			call_user_func_array('sprintf', array_merge(array($this->sMessage),$this->arrMessageArgs)) ;
 	}
 	
 	public function setUrl($sUrl)
@@ -107,7 +124,8 @@ class Relocater extends Controller
 	}
 	
 	private $sUrl ;
-	private $sTitle ;
+	private $sMessage ;
+	private $arrMessageArgs ;
 	private $nWaitSec ;
 }
 
