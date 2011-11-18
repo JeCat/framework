462ea369d84eabd9d437918bacf3155d799b3900
lexdene <elephant_liu@mail.dlut.edu.cn>
2011-11-18 16:05:48 +0800

    show and hide menu by js

--- src/public/js/mvc/view/widget/menu/base.js
+++ src/public/js/mvc/view/widget/menu/base.js
@@ -13,15 +13,13 @@ jc.mvc.view.widget.menu.base.getStyle=function(o,styleName){
 	}
 	return y;
 }
+jc.mvc.view.widget.menu.base.arrActive=new Array();
 jc.mvc.view.widget.menu.base.show=function(o){
-	console.log(o.parentNode);
-	console.log(this.getStyle(o.parentNode,'width'));
 	o.parentNode.style.position="relative";
 	o.style.position="absolute";
 	o.style.left = this.getStyle(o.parentNode,'width');
-	console.log(o.style.left);
 	o.style.top = 0;
-	o.style.display="block";	
+	o.style.display="block";
 }
 jc.mvc.view.widget.menu.base.hide=function(o){
 	o.style.display="none";
@@ -29,24 +27,59 @@ jc.mvc.view.widget.menu.base.hide=function(o){
 jc.mvc.view.widget.menu.base.isShow=function(o){
 	return this.getStyle(o,'display')== "block";
 }
+jc.mvc.view.widget.menu.base.isAlone=function(o){
+	return ( o.className && ( o.className.match('jc-mvc-view-widget-menu-menu-alone') == 'jc-mvc-view-widget-menu-menu-alone'));
+}
 jc.mvc.view.widget.menu.base.expand=function(o){
-	console.log(o);
 	var childlist = o.childNodes;
 	for(var i=0;i<childlist.length;++i){
-		console.log("className="+childlist[i].className);
-		if(childlist[i].className && childlist[i].className.match('jc-mvc-view-widget-menu-menu-alone')){
+		if(this.isAlone(childlist[i])){
+			o = childlist[i];
+			this.show(o);
+			this.arrActive.push(o.id);
+		}
+	}
+}
+jc.mvc.view.widget.menu.base.contract=function(o){
+	var childlist = o.childNodes;
+	for(var i=0;i<childlist.length;++i){
+		if(this.isAlone(childlist[i])){
 			var o = childlist[i];
-			if(this.isShow(o)){
-				this.hide(o);
-			}else{
-				this.show(o);
-			}
+			this.hide(o);
+		}
+	}
+}
+jc.mvc.view.widget.menu.base.isOrParentOf=function(a , b){// a is b or a is parent of b
+	while(b && typeof(b)!='undefined'){
+		if(a == b) return true;
+		b = b.parentNode;
+	}
+	return false;
+}
+jc.mvc.view.widget.menu.base.hideActive=function(item){//隐藏所有和item无关的menu
+	for(var i=0;i<this.arrActive.length;++i){
+		a=this.arrActive[i];
+		if(! this.isOrParentOf(document.getElementById(a) ,item) ){
+			this.hide(document.getElementById(a));
+			this.arrActive.splice(i,1);
 		}
 	}
 }
 jc.mvc.view.widget.menu.base.item_onActive=function(item){
+	clearTimeout(this.t);
+	this.t=-1;
+	this.hideActive(item);
 	this.expand(item);
 	this.stopBubble(event);
+}
+jc.mvc.view.widget.menu.base.t=-1;
+jc.mvc.view.widget.menu.base.timeout=function(){
+	this.hideActive(null);
+}
+jc.mvc.view.widget.menu.base.item_onDisactive=function(item){
+	if(this.t == -1){
+		this.t=setTimeout("jc.mvc.view.widget.menu.base.timeout()",500);
+	}
 }
 jc.mvc.view.widget.menu.base.stopBubble=function(e) {  
     var e = e ? e : window.event;
