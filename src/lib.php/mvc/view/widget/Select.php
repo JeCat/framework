e1bf4a9d0e5178e7a4253e5e3a84086a240dbb0b
kongyuan <anubiskong@gmail.com>
2011-11-24 14:51:15 +0800

    ...

--- src/lib.php/mvc/view/widget/Select.php
+++ src/lib.php/mvc/view/widget/Select.php
@@ -2,7 +2,6 @@
 namespace jc\mvc\view\widget;
 
 use jc\verifier\Length;
-
 use jc\lang\Exception;
 use jc\mvc\view\IView;
 
@@ -27,8 +26,15 @@ class Select extends FormWidget {
 		}
 	}
 	
-	//增加option条目 
-	//selected 该选项是否默认选中	
+	
+	/**
+	 * 增加option条目 
+	 * selected 该选项是否默认选中
+	 * @param string $sText option显示的文字
+	 * @param mixed $sValue  option的值
+	 * @param bool $bSelected 是否被选中,默认false
+	 * @return Select
+	 */
 	public function addOption($sText, $sValue=null, $bSelected=false )
 	{
 		$this->arrOptions [] = Array ($sValue, $sText, $bSelected );
@@ -51,14 +57,14 @@ class Select extends FormWidget {
 	}
 	
 	public function unsetSelected(){
-		foreach($this->arrOptions as $value){
+		foreach($this->arrOptions as &$value){
 			$value[2] = false;
 		}
 	}
 	
 	public function getSelected(){
 		$arrSelected = array();
-		foreach($this->arrOptions as $value){
+		foreach($this->arrOptions as &$value){
 			if($value[2] == true){
 				$arrSelected = $value;
 			}
@@ -108,7 +114,12 @@ class Select extends FormWidget {
 		}
 		return $this->arrOptions[$nIndex][0];
 	}
-	//取得option列表
+	/**
+	 * 
+	 * 取得option列表的迭代器
+	 *
+	 * @return \jc\pattern\iterate\ArrayIterator
+	 */
 	public function optionIterator() {
 		return new \jc\pattern\iterate\ArrayIterator ( $this->arrOptions );
 	}
