cea46022afd7385fb834010d14d55446dd20ed15
alee <zq@alee2002.com>
2012-01-05 10:46:08 +0800

    增加了 Authorizer/Permission 等相关的类

---
+++ class/auth/CallbackPermission.php
@@ -0,0 +1,19 @@
+<?php
+namespace org\jecat\framework\auth ;
+
+class CallbackPermission implements IPermission
+{
+	public function __construct($callback)
+	{
+		$this->callback = $callback ;
+	}
+	
+	public function check(IdManager $aIdManager)
+	{
+		return call_user_func_array($this->callback, array($aIdManager))!==false ;
+	}
+	
+	private $callback ;
+}
+
+?>
