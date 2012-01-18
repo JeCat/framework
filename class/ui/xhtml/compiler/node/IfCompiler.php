9cd5f258dc94754772899e129482de24716401c9
aleechou <zq@alee2002.com>
2012-01-18 23:59:52 +0800

    一些注释文档

--- class/ui/xhtml/compiler/node/IfCompiler.php
+++ class/ui/xhtml/compiler/node/IfCompiler.php
@@ -11,6 +11,29 @@ use org\jecat\framework\ui\IObject;
 use org\jecat\framework\ui\xhtml\compiler\NodeCompiler;
 
 /**
+ * @wiki /模板引擎/标签
+ *
+ * {|
+ *  !<if>
+ *  !不可单行
+ *  !条件流程控制，匿名属性必须是一个表达式，当表达式返回true时，执行 <if> 和 </if> 之间的内容
+ *  |---
+ *  !属性
+ *  !
+ *  !类型
+ *  !默认值
+ *  !说明
+ *  |---
+ *  |@匿名
+ *  |必须
+ *  |expression
+ *  |
+ *  |条件表达式
+ *  |}
+ */
+
+
+/**
  * @example /模板/标签/自定义标签:name[1]
  * @forwiki /模板/标签/自定义标签
  *
