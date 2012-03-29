c8595a3444265f359af819dafdbcfa8eea40aafc
aleechou <zq@alee2002.com>
2012-03-29 22:10:18 +0800

    修改 BaseParserFactory , 可以独立的创建sql子句的parser

--- class/db/sql2/parser/BaseParserFactory.php
+++ class/db/sql2/parser/BaseParserFactory.php
@@ -9,11 +9,14 @@ use org\jecat\framework\lang\Object;
  */
 class BaseParserFactory extends Object
 {	
-	public function create($bShare=true,Dialect $aDialect=null)
+	/**
+	 * @return AbstractParser
+	 */
+	public function create($bShare=true,Dialect $aDialect=null,$sAction='statement')
 	{
-		if( $bShare and $this->aShareParser )
+		if( $bShare and isset($this->arrShareParsers[$sAction]) )
 		{
-			return $this->aShareParser ;
+			return $this->arrShareParsers[$sAction] ;
 		}
 		
 		if(!$aDialect)
@@ -21,66 +24,84 @@ class BaseParserFactory extends Object
 			$aDialect = Dialect::singleton() ;
 		}
 		
-		$aParser = new AbstractParser($aDialect) ;
-		
-		$aParser->setDialect($aDialect)
-		
-				// SELECT 子句
-				->addChildState(
-					self::createParser('ClauseParser',$aDialect,'select') 
-						->addChildState( self::createParser('ColumnParser',$aDialect) )
-				)
+		switch($sAction)
+		{
+			case 'statement' :
+				$aParser = self::createParserInstace('AbstractParser',$aDialect)
+								->addChildState($this->create($bShare,$aDialect,'select'))
+								->addChildState($this->create($bShare,$aDialect,'from'))
+								->addChildState($this->create($bShare,$aDialect,'where')) 
+								->addChildState($this->create($bShare,$aDialect,'group'))
+								->addChildState($this->create($bShare,$aDialect,'order'))
+								->addChildState($this->create($bShare,$aDialect,'limit')) ; 
+				break ;
 				
-				// FROM 子句
-				->addChildState(
-					self::createParser('ClauseParser',$aDialect,'from') 
-						->addChildState( self::createParser('SubQueryParser',$aDialect,$aParser) )
-						->addChildState(self::createParser('TableParser',$aDialect))
-						
-						// JOIN 子句
-						->addChildState(
-								self::createParser('TableJoinParser',$aDialect)
-									->addChildState(self::createParser('TableParser',$aDialect))
-									// ON 子句
-									->addChildState(
-										self::createParser('ClauseParser',$aDialect,'on') 
-											->addChildState(self::createParser('ColumnParser',$aDialect))
-									)
-									// USING 子句
-									->addChildState(
-										self::createParser('ClauseParser',$aDialect,'using') 
-											->addChildState(self::createParser('ColumnParser',$aDialect))
-									)
-						)
-				)
+			case 'select' :
+				$aParser = self::createParserInstace('ClauseParser',$aDialect,'select')
+								->addChildState($this->create($bShare,$aDialect,'column')) ;
+				break ;
 				
-				// WHERE 子句
-				->addChildState(
-						self::createParser('ClauseParser',$aDialect,'where')
-							->addChildState(self::createParser('ColumnParser',$aDialect))
-				)
-				// GROUP 子句
-				->addChildState(
-					self::createParser('ClauseParser',$aDialect,'group') 
-							->addChildState(self::createParser('ColumnParser',$aDialect))
-				)
-				// ORDER 子句
-				->addChildState(
-					self::createParser('ClauseParser',$aDialect,'order') 
-							->addChildState(self::createParser('ColumnParser',$aDialect))
-				)
-				// LIMIT 子句
-				->addChildState(
-					self::createParser('ClauseParser',$aDialect,'limit') 
-							->addChildState(self::createParser('ColumnParser',$aDialect))
-				) ;
-	
+			case 'from' :
+				$aParser = self::createParserInstace('ClauseParser',$aDialect,'from') 
+							->addChildState($this->create($bShare,$aDialect,'subquery') )
+							->addChildState($this->create($bShare,$aDialect,'table'))
+							->addChildState($this->create($bShare,$aDialect,'join'))  ;
+				break ;
+				
+			case 'where' :
+				$aParser = self::createParserInstace('ClauseParser',$aDialect,'where')
+								->addChildState($this->create($bShare,$aDialect,'table')) ;
+				break ;
+				
+			case 'group' :
+				$aParser = self::createParserInstace('ClauseParser',$aDialect,'group')
+								->addChildState($this->create($bShare,$aDialect,'table')) ;
+				break ;
+				
+			case 'order' :
+				$aParser = self::createParserInstace('ClauseParser',$aDialect,'order')
+								->addChildState($this->create($bShare,$aDialect,'table')) ;
+				break ;
+				
+			case 'limit' :
+				$aParser = self::createParserInstace('ClauseParser',$aDialect,'limit')
+								->addChildState($this->create($bShare,$aDialect,'table')) ;
+				break ;
+				
+			case 'on' :
+				$aParser = self::createParserInstace('ClauseParser',$aDialect,'on') 
+								->addChildState($this->create($bShare,$aDialect,'table')) ;
+				break ;
+				
+			case 'using' :
+				$aParser = self::createParserInstace('ClauseParser',$aDialect,'using') 
+								->addChildState($this->create($bShare,$aDialect,'table')) ;
+				break ;
+				
+			case 'column' :
+				$aParser = self::createParserInstace('ColumnParser',$aDialect) ;
+				break ;
+				
+			case 'table' :
+				$aParser = self::createParserInstace('TableParser',$aDialect) ;
+				break ;
+				
+			case 'join' :
+				$aParser = self::createParserInstace('TableJoinParser',$aDialect)
+								->addChildState($this->create($bShare,$aDialect,'table'))
+								->addChildState($this->create($bShare,$aDialect,'on'))
+								->addChildState($this->create($bShare,$aDialect,'using')) ;
+				break ;
+				
+			case 'subquery' :
+				$aParser = self::createParserInstace('SubQueryParser',$aDialect) ;
+				break ;
+		}
 		
 		if( $bShare )
 		{
-			$this->aShareParser = $aParser ;
+			$this->arrShareParsers[$sAction] = $aParser ;
 		}
-		
 		
 		return $aParser ;
 	}
@@ -88,7 +109,71 @@ class BaseParserFactory extends Object
 	/**
 	 * @return AbstractParser
 	 */
-	static private function createParser($sClass,Dialect $aDialect,$argvs=null,$sNamespace=__NAMESPACE__)
+	public function createParserSelect($bShare=true,Dialect $aDialect=null)
+	{
+		return $this->create($bShare,$aDialect,'select') ;
+	}
+	/**
+	 * @return AbstractParser
+	 */
+	public function createParserFrom($bShare=true,Dialect $aDialect=null)
+	{ return $this->create($bShare,$aDialect,'from') ; }
+	/**
+	 * @return AbstractParser
+	 */
+	public function createParserWhere($bShare=true,Dialect $aDialect=null)
+	{ return $this->create($bShare,$aDialect,'where') ; }
+	/**
+	 * @return AbstractParser
+	 */
+	public function createParserOrder($bShare=true,Dialect $aDialect=null)
+	{ return $this->create($bShare,$aDialect,'order') ; }
+	/**
+	 * @return AbstractParser
+	 */
+	public function createParserGroup($bShare=true,Dialect $aDialect=null)
+	{ return $this->create($bShare,$aDialect,'group') ; }
+	/**
+	 * @return AbstractParser
+	 */
+	public function createParserLimit($bShare=true,Dialect $aDialect=null)
+	{ return $this->create($bShare,$aDialect,'limit') ; }
+	/**
+	 * @return AbstractParser
+	 */
+	public function createParserOn($bShare=true,Dialect $aDialect=null)
+	{ return $this->create($bShare,$aDialect,'on') ; }
+	/**
+	 * @return AbstractParser
+	 */
+	public function createParserUsing($bShare=true,Dialect $aDialect=null)
+	{ return $this->create($bShare,$aDialect,'Using') ; }
+	/**
+	 * @return AbstractParser
+	 */
+	public function createParserColumn($bShare=true,Dialect $aDialect=null)
+	{ return $this->create($bShare,$aDialect,'column') ; }
+	/**
+	 * @return AbstractParser
+	 */
+	public function createParserTable($bShare=true,Dialect $aDialect=null)
+	{ return $this->create($bShare,$aDialect,'table') ; }
+	/**
+	 * @return AbstractParser
+	 */
+	public function createParserSubquery($bShare=true,Dialect $aDialect=null)
+	{ return $this->create($bShare,$aDialect,'suquery') ; }
+	/**
+	 * @return AbstractParser
+	 */
+	public function createParserJoin($bShare=true,Dialect $aDialect=null)
+	{ return $this->create($bShare,$aDialect,'join') ; }
+	
+	
+	/**
+	 * @return AbstractParser
+	 */
+	static private function createParserInstace($sClass,Dialect $aDialect,$argvs=null,$sNamespace=__NAMESPACE__)
 	{
 		$sClass = $sNamespace.'\\'.$sClass ;
 		$aParser = $sClass::createInstance($argvs) ;
@@ -98,7 +183,7 @@ class BaseParserFactory extends Object
 		return $aParser ;
 	}
 	
-	private $aShareParser ;
+	private $arrShareParsers ;
 }
 
 ?>
