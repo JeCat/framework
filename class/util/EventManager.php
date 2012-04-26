fc840a81c3c8e12890ab75903534d9731f849323
alee <zq@alee2002.com>
2012-04-26 11:27:12 +0800

    EventManager::serialize() 检查事件回调函数的array格式

--- class/util/EventManager.php
+++ class/util/EventManager.php
@@ -67,6 +67,10 @@ class EventManager extends Object implements \Serializable
 				// 方法
 				if( is_array($fnHandler) )
 				{
+					if( empty($fnHandler[0]) or empty($fnHandler[1]) )
+					{
+						throw new \Exception("事件回调函数类型错误：%s",var_export($fnHandler,1)) ;
+					}
 					// 对像方法
 					if(is_object($fnHandler[0]))
 					{
