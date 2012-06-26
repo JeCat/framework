d75e5cbea89b32afc5e9c640211d29449581fe31
gaojun <gaojun@zhenzhi.com>
2012-06-26 10:56:54 +0800

    1

--- class/mvc/model/Model.php
+++ class/mvc/model/Model.php
@@ -319,7 +319,7 @@ class Model implements \Iterator, \ArrayAccess, \Serializable, IPaginal
 	        $arr_field[] = $row[$field];
 	    }
 	    
-	    if(!empty($arr_field))
+	    if(is_array($arr_field) && is_array($this->arrData))
 	    {
 	        array_multisort($arr_field,$sort_type,$this->arrData);
 	    }
