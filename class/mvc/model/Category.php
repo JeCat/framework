3ccc613585e3f1ecb6f130a8835a620ffd4ddf1c
kongyuan <anubiskong@gmail.com>
2012-10-08 10:55:52 +0800

    ...

--- class/mvc/model/Category.php
+++ class/mvc/model/Category.php
@@ -434,9 +434,11 @@ class Category extends Object
 		}
 		
 		$aParentsModelList = Model::create($aModel->prototype());
+
 		$aParentsModelList->order('lft');
 		$aParentsModelList->where("lft < {$aModel['lft']} and rgt > {$aModel['rgt']}");
 		$aParentsModelList->load();
+
 		return $aParentsModelList;
 	}
 
