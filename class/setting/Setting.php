69c9243014bc0f3e786efb901648940aeabc5606
lexdene <elephant_liu@mail.dlut.edu.cn>
2012-10-08 09:45:49 +0800

    tilu 1

--- class/setting/Setting.php
+++ class/setting/Setting.php
@@ -171,7 +171,12 @@ abstract class Setting extends Object implements ISetting
 			$sInMountPath = substr($sKey,strlen($sFindMount));
 			return $this->getMountSettingByPath($sFindMount)->value($sInMountPath,$defaultValue);
 		}
-		return $this->valuePri($sKey,$defaultValue);
+		if( $this->hasValuePri($sKey) ){
+			return $this->valuePri($sKey,$defaultValue);
+		}else{
+			$this->setValuePri($sKey,$defaultValue);
+			return $defaultValue;
+		}
 	}
 	
 	public function setValue($sKey,$value){
